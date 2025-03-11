98c0b0dd8cc3d211e455d1ff5dea4f233be1674b
pqhung3007 <hungpham307@gmail.com>
2025-03-11 16:06:09 +0700

    s6 connect to db

--- s6-env-and-secrets/data/database.js
+++ s6-env-and-secrets/data/database.js
@@ -1,4 +1,7 @@
 import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
